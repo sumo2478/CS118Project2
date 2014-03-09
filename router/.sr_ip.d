0b152efccd7b850f362ce1d20460e6c215be5fc6
Collin Yen <collinyen7@gmail.com>
2014-03-09 16:58:56 -0700

    Implemented retrieving destination node for ip routing

--- router/.sr_ip.d
+++ router/.sr_ip.d
@@ -1,1 +1,1 @@
-sr_ip.o: sr_ip.c sr_protocol.h sr_router.h sr_arpcache.h sr_if.h
+sr_ip.o: sr_ip.c sr_protocol.h sr_router.h sr_arpcache.h sr_if.h sr_rt.h
--- router/sr_ip.c
+++ router/sr_ip.c
@@ -3,6 +3,7 @@
 
 #include "sr_protocol.h"
 #include "sr_router.h"
+#include "sr_rt.h"
 
 int handle_ip(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* interface)
 {
@@ -16,8 +17,27 @@ int handle_ip(struct sr_instance* sr, uint8_t* packet, unsigned int len, char* i
 
 	struct sr_ip_hdr* ip_header = (struct sr_ip_hdr*) (packet + sizeof(struct sr_ethernet_hdr));
 
+	/* Determine the next ip destination */
 	struct sr_rt* routing_node = sr->routing_table;
+	unsigned long max_mask = 0; 		   /* The closest anded mask to the current packet */
+	struct sr_rt* destination_node = NULL; /* The destination node that should be sent to given the routing table */
+
+	while(routing_node)
+	{
+		/* If the masked address is the closest match then set it to the destination node */
+		unsigned long current_mask = routing_node->mask.s_addr & ip_header->ip_dst;
+		if (current_mask > max_mask)
+		{
+			max_mask = current_mask;
+			destination_node = routing_node;
+		}
+
+		routing_node = routing_node->next;
+	}
+
+
 
 
     printf("Handling IP packet\n");
+    return 0;
 }
